0e823e15b13af1618c9255abeca31c3f440ea5ee
Jack Tanner <jack@gimly.io>
2021-05-06 16:16:51 +0200

    Added todo functions

--- src/resolver.ts
+++ src/resolver.ts
@@ -1,15 +1,32 @@
 import { DIDDocument, DIDResolutionOptions, DIDResolutionResult, ParsedDID, Resolver } from "did-resolver"
 
+function checkDID(parsed: ParsedDID) {
+    // TODO check DID is valid format and structure
+}
+
+async function fetchAccount(did: string, parsed: ParsedDID, options: DIDResolutionOptions): Promise<object> {
+    // Find the API from registered eosio chains or from options
+
+    // Fetch the eosio account
+    return {};
+}
+
+function createDIDDocument(eosioAccount: any): DIDDocument {
+    // TODO
+    return { id: 'did:eosio:stub' };
+}
+
 export async function resolve(
     did: string,
     parsed: ParsedDID,
     didResolver: Resolver,
     options: DIDResolutionOptions): Promise<DIDResolutionResult> {
 
-    console.log(did, parsed, didResolver, options);
+    checkDID(parsed);
 
-    // TODO
-    const didDoc: DIDDocument = { id: 'did:eosio:stub' };
+    const eosioAccount = await fetchAccount(did, parsed, options);
+
+    const didDoc = createDIDDocument(eosioAccount);
 
     return {
         didResolutionMetadata: { contentType: 'application/did+ld+json' },
