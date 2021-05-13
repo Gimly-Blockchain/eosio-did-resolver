b148f8a943cdddf8f3e088deac4fba19e53916e2
Jonas Walter <balurat@gmail.com>
2021-05-13 18:49:55 +0200

    Implemented checkDID()
    RegEx check to validate the method-specific-id.
    Fetch registry entry for the chain.

--- src/resolver.ts
+++ src/resolver.ts
@@ -1,10 +1,53 @@
 import { DIDDocument, DIDResolutionOptions, DIDResolutionResult, ParsedDID, Resolver } from "did-resolver"
+const registry = require('../eosio-did-chain-registry.json');
 
-function checkDID(parsed: ParsedDID) {
-    // TODO check DID is valid format and structure
+const SUBJECT_ID = `([a-z1-5.]{0,12}[a-z1-5])`;
+const CHAIN_ID   = new RegExp( `^([A-Fa-f0-9]{64}):${SUBJECT_ID}$` )
+const CHAIN_NAME = new RegExp(
+    `^(([a-z1-5.]{0,12}[a-z1-5])((:[a-z1-5.]{0,12}[a-z1-5])+)?):${SUBJECT_ID}$`
+)
+
+function findChainByID(chainId: string) {
+    registry.map((chain: any, chainName: string) => {
+        if(chain.chainId === chainId) return chain;
+    });
 }
 
-async function fetchAccount(did: string, parsed: ParsedDID, options: DIDResolutionOptions): Promise<object> {
+function findChainByName(chainName: string) {
+    return registry[chainName];
+}
+
+/**
+ * Checks the method-specific-id, parses it into chainId/chainName 
+ * and subject (account name) and fetches the correlating chain from 
+ * the registry.
+ * If the method-specific-id is invalid or no matching chain was 
+ * found, then an empty object is returned.
+ * 
+ * @param parsed 
+ * @returns { {chain?: object, subject?: string} }
+ */
+function checkDID(parsed: ParsedDID) {
+    const partsID = parsed.id.match(CHAIN_ID);
+    if(partsID) {
+        return {
+            chain: findChainByID(partsID[1]),
+            subject: partsID[partsID.length - 1]
+        }
+    }
+
+    const partsName = parsed.id.match(CHAIN_NAME);
+    if(partsName) {
+        return {
+            chain: findChainByName(partsName[1]),
+            subject: partsName[partsName.length - 1]
+        }
+    }
+
+    return {}
+}
+
+async function fetchAccount(registryEntry: object, did: string, parsed: ParsedDID, options: DIDResolutionOptions): Promise<object> {
     // Find the API from registered eosio chains or from options
 
     // Fetch the eosio account
@@ -22,9 +65,9 @@ export async function resolve(
     didResolver: Resolver,
     options: DIDResolutionOptions): Promise<DIDResolutionResult> {
 
-    checkDID(parsed);
+    const registryEntry = checkDID(parsed);
 
-    const eosioAccount = await fetchAccount(did, parsed, options);
+    const eosioAccount = await fetchAccount(registryEntry, did, parsed, options);
 
     const didDoc = createDIDDocument(eosioAccount);
 
