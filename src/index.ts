3c275e09fb2aa927d620b142c2886670d11b7469
Jonas Walter <balurat@gmail.com>
2021-05-14 04:04:53 +0200

    User supplied chain-name-registry extension
    Possibility to pass a custom registry object to the resolver.
    Cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,15 @@
-import { ResolverRegistry } from "did-resolver";
-import { resolve } from './resolver';
+import { ResolverRegistry, DIDResolutionOptions, DIDResolutionResult, ParsedDID, Resolver } from "did-resolver";
+import { resolve, Registry } from './resolver';
 
-export function getResolver(): ResolverRegistry {
-  return { 'eosio': resolve }
+export function getResolver(registry?: Registry): ResolverRegistry {
+  return { 
+    'eosio': async (
+                did: string,
+                parsed: ParsedDID,
+                didResolver: Resolver,
+                options: DIDResolutionOptions
+              ): Promise<DIDResolutionResult> => { 
+                return resolve(did, parsed, didResolver, options, registry) 
+              } 
+  }
 }
--- src/resolver.ts
+++ src/resolver.ts
@@ -1,5 +1,6 @@
 import { DIDDocument, DIDResolutionOptions, DIDResolutionResult, ParsedDID, Resolver } from "did-resolver"
-const registry = require('../eosio-did-chain-registry.json');
+
+const defaultRegistry: Registry = require('../eosio-did-chain-registry.json');
 
 const ERROR_RESULT = {
     didResolutionMetadata: { error: 'invalidDid' },
@@ -13,47 +14,36 @@ const CHAIN_NAME = new RegExp(
     `^(([a-z1-5.]{0,12}[a-z1-5])((:[a-z1-5.]{0,12}[a-z1-5])+)?):${SUBJECT_ID}$`
 )
 
-function findChainByID(chainId: string) {
-    registry.map((chain: any, chainName: string) => {
-        if(chain.chainId === chainId) return chain;
-    });
-}
+function checkDID(parsed: ParsedDID, registry: Registry): MethodId | undefined {
 
-function findChainByName(chainName: string) {
-    return registry[chainName];
-}
-
-/**
- * Checks the method-specific-id, parses it into chainId/chainName 
- * and subject (account name) and fetches the correlating chain from 
- * the registry.
- * If the method-specific-id is invalid or no matching chain was 
- * found, then an empty object is returned.
- * 
- * @param parsed 
- * @returns { {chain?: object, subject?: string} }
- */
-function checkDID(parsed: ParsedDID) {
-    const partsID = parsed.id.match(CHAIN_ID);
-    if(partsID) {
-        return {
-            chain: findChainByID(partsID[1]),
-            subject: partsID[partsID.length - 1]
-        }
-    }
-
+    // findChainByName
     const partsName = parsed.id.match(CHAIN_NAME);
     if(partsName) {
-        return {
-            chain: findChainByName(partsName[1]),
+        const entry = registry[partsName[1]];
+        if(entry) return {
+            chain: entry,
             subject: partsName[partsName.length - 1]
+        }
+        return undefined;
+    }
+    
+    // findChainById
+    const partsID = parsed.id.match(CHAIN_ID);
+    if(partsID) {
+        for (let key of Object.keys(registry)) {
+            const entry: Entry = registry[key];
+            if(entry.chainId === partsID[1]) 
+                return { 
+                    chain: entry, 
+                    subject: partsID[partsID.length - 1] 
+                };
         }
     }
 
-    return {}
+    return undefined
 }
 
-async function fetchAccount(registryEntry: object, did: string, parsed: ParsedDID, options: DIDResolutionOptions): Promise<object> {
+async function fetchAccount(methodId: MethodId, did: string, parsed: ParsedDID, options: DIDResolutionOptions): Promise<object> {
     // Find the API from registered eosio chains or from options
 
     // Fetch the eosio account
@@ -69,15 +59,22 @@ export async function resolve(
     did: string,
     parsed: ParsedDID,
     didResolver: Resolver,
-    options: DIDResolutionOptions): Promise<DIDResolutionResult> {
+    options: DIDResolutionOptions,
+    customRegistry?: Registry): Promise<DIDResolutionResult> {
+    
+    const registry: Registry = {
+        ...defaultRegistry, 
+        ...customRegistry
+    };
 
-    const registryEntry = checkDID(parsed);
+    const methodId = checkDID(parsed, registry);
 
-    if(!registryEntry.chain) {
+    if(!methodId) {
+        // invalid method-specific-id OR no matching chain in the registry
         return ERROR_RESULT;
     }
 
-    const eosioAccount = await fetchAccount(registryEntry, did, parsed, options);
+    const eosioAccount = await fetchAccount(methodId, did, parsed, options);
 
     const didDoc = createDIDDocument(eosioAccount);
 
@@ -86,4 +83,24 @@ export async function resolve(
         didDocument: didDoc,
         didDocumentMetadata: {}
     }
+}
+
+interface Service {
+    id: string,
+    type: string|string[],
+    serviceEndpoint: string
+}
+
+interface Entry {
+    chainId: string,
+    service: Service[]
+}
+
+export interface Registry {
+    [chainName: string]: Entry;
+}
+
+interface MethodId {
+    chain: Entry,
+    subject: string
 }
