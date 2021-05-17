e765de7d4f90e0d3fc75388eeba1af656881947a
jay <j.rahaus@online.de>
2021-05-17 15:05:30 +0200

    using prettier with config from package.json

--- src/resolver.ts
+++ src/resolver.ts
@@ -1,106 +1,116 @@
-import { DIDDocument, DIDResolutionOptions, DIDResolutionResult, ParsedDID, Resolver } from "did-resolver"
+import {
+  DIDDocument,
+  DIDResolutionOptions,
+  DIDResolutionResult,
+  ParsedDID,
+  Resolver,
+} from 'did-resolver';
 
 const eosioChainRegistry: Registry = require('../eosio-did-chain-registry.json');
 
 const ERROR_RESULT = {
-    didResolutionMetadata: { error: 'invalidDid' },
-    didDocument: null,
-    didDocumentMetadata: {}
+  didResolutionMetadata: { error: 'invalidDid' },
+  didDocument: null,
+  didDocumentMetadata: {},
 };
 
 const SUBJECT_ID = `([a-z1-5.]{0,12}[a-z1-5])`;
-const CHAIN_ID   = new RegExp( `^([A-Fa-f0-9]{64}):${SUBJECT_ID}$` )
+const CHAIN_ID = new RegExp(`^([A-Fa-f0-9]{64}):${SUBJECT_ID}$`);
 const CHAIN_NAME = new RegExp(
-    `^(([a-z1-5.]{0,12}[a-z1-5])((:[a-z1-5.]{0,12}[a-z1-5])+)?):${SUBJECT_ID}$`
-)
+  `^(([a-z1-5.]{0,12}[a-z1-5])((:[a-z1-5.]{0,12}[a-z1-5])+)?):${SUBJECT_ID}$`
+);
 
 function checkDID(parsed: ParsedDID, registry: Registry): MethodId | undefined {
+  // findChainByName
+  const partsName = parsed.id.match(CHAIN_NAME);
+  if (partsName) {
+    const entry = registry[partsName[1]];
+    if (entry)
+      return {
+        chain: entry,
+        subject: partsName[partsName.length - 1],
+      };
+    return undefined;
+  }
 
-    // findChainByName
-    const partsName = parsed.id.match(CHAIN_NAME);
-    if(partsName) {
-        const entry = registry[partsName[1]];
-        if(entry) return {
-            chain: entry,
-            subject: partsName[partsName.length - 1]
-        }
-        return undefined;
+  // findChainById
+  const partsID = parsed.id.match(CHAIN_ID);
+  if (partsID) {
+    for (let key of Object.keys(registry)) {
+      const entry: Entry = registry[key];
+      if (entry.chainId === partsID[1])
+        return {
+          chain: entry,
+          subject: partsID[partsID.length - 1],
+        };
     }
-    
-    // findChainById
-    const partsID = parsed.id.match(CHAIN_ID);
-    if(partsID) {
-        for (let key of Object.keys(registry)) {
-            const entry: Entry = registry[key];
-            if(entry.chainId === partsID[1]) 
-                return { 
-                    chain: entry, 
-                    subject: partsID[partsID.length - 1] 
-                };
-        }
-    }
+  }
 
-    return undefined
+  return undefined;
 }
 
-async function fetchAccount(methodId: MethodId, did: string, parsed: ParsedDID, options: DIDResolutionOptions): Promise<object> {
-    // Find the API from registered eosio chains or from options
+async function fetchAccount(
+  methodId: MethodId,
+  did: string,
+  parsed: ParsedDID,
+  options: DIDResolutionOptions
+): Promise<object> {
+  // Find the API from registered eosio chains or from options
 
-    // Fetch the eosio account
-    return {};
+  // Fetch the eosio account
+  return {};
 }
 
 function createDIDDocument(eosioAccount: any): DIDDocument {
-    // TODO
-    return { id: 'did:eosio:stub' };
+  // TODO
+  return { id: 'did:eosio:stub' };
 }
 
 export async function resolve(
-    did: string,
-    parsed: ParsedDID,
-    didResolver: Resolver,
-    options: DIDResolutionOptions
-    ): Promise<DIDResolutionResult> {
-    
-    const registry: Registry = {
-        ...eosioChainRegistry,
-        ...options.eosioChainRegistry
-    };
+  did: string,
+  parsed: ParsedDID,
+  didResolver: Resolver,
+  options: DIDResolutionOptions
+): Promise<DIDResolutionResult> {
+  const registry: Registry = {
+    ...eosioChainRegistry,
+    ...options.eosioChainRegistry,
+  };
 
-    const methodId = checkDID(parsed, registry);
+  const methodId = checkDID(parsed, registry);
 
-    if(!methodId) {
-        // invalid method-specific-id OR no matching chain in the registry
-        return ERROR_RESULT;
-    }
+  if (!methodId) {
+    // invalid method-specific-id OR no matching chain in the registry
+    return ERROR_RESULT;
+  }
 
-    const eosioAccount = await fetchAccount(methodId, did, parsed, options);
+  const eosioAccount = await fetchAccount(methodId, did, parsed, options);
 
-    const didDoc = createDIDDocument(eosioAccount);
+  const didDoc = createDIDDocument(eosioAccount);
 
-    return {
-        didResolutionMetadata: { contentType: 'application/did+ld+json' },
-        didDocument: didDoc,
-        didDocumentMetadata: {}
-    }
+  return {
+    didResolutionMetadata: { contentType: 'application/did+ld+json' },
+    didDocument: didDoc,
+    didDocumentMetadata: {},
+  };
 }
 
 interface Service {
-    id: string,
-    type: string|string[],
-    serviceEndpoint: string
+  id: string;
+  type: string | string[];
+  serviceEndpoint: string;
 }
 
 interface Entry {
-    chainId: string,
-    service: Service[]
+  chainId: string;
+  service: Service[];
 }
 
 export interface Registry {
-    [chainName: string]: Entry;
+  [chainName: string]: Entry;
 }
 
 interface MethodId {
-    chain: Entry,
-    subject: string
+  chain: Entry;
+  subject: string;
 }
--- test/resolver.test.ts
+++ test/resolver.test.ts
@@ -1,4 +1,4 @@
-import { Resolver } from 'did-resolver'
+import { Resolver } from 'did-resolver';
 import { getResolver } from '../src/index';
 
 describe('EOSIO resolver', async () => {
@@ -9,21 +9,22 @@ describe('EOSIO resolver', async () => {
     console.log('Resolving ' + eoscanadacomDid);
     const eosDidDocument = await resolver.resolve(eoscanadacomDid);
     console.log('DID Document:', eosDidDocument);
-  })
+  });
 
   it('Resolve a jungle testnet DID Document', async () => {
     const jungleDid = 'did:eosio:eos:testnet:jungle:lioninjungle';
     console.log('Resolving ' + jungleDid);
     const jungleDidDocument = await resolver.resolve(jungleDid);
     console.log('DID Document:', jungleDidDocument);
-  })
+  });
 
   it('Resolve a chain-id based DID Document', async () => {
-    const telosDid = 'did:eosio:4667b205c6838ef70ff7988f6e8257e8be0e1284a2f59699054a018f743b1d11:caleosblocks';
+    const telosDid =
+      'did:eosio:4667b205c6838ef70ff7988f6e8257e8be0e1284a2f59699054a018f743b1d11:caleosblocks';
     console.log('Resolving ' + telosDid);
     const telosDidDocument = await resolver.resolve(telosDid);
     console.log('DID Document:', telosDidDocument);
-  })
+  });
 
   it('Fail to resolve an invalid DID', async () => {
     const invalidDid = 'did:eosio:eos:aaaaaaaaaaaaaaaaaaaa';
@@ -34,7 +35,7 @@ describe('EOSIO resolver', async () => {
     } catch (e) {
       console.log('Error resolving DID:', e.what());
     }
-  })
+  });
 
   it('Fail to resolve a non existant DID', async () => {
     const nonExistantDid = 'did:eosio:telos:aaaaa';
@@ -45,6 +46,5 @@ describe('EOSIO resolver', async () => {
     } catch (e) {
       console.log('Error resolving DID:', e.what());
     }
-  })
-
+  });
 });
