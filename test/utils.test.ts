c01cefb4b32538c8f729b0191f6f86148e7a2471
Jack Tanner <jack@gimly.io>
2021-06-22 16:34:59 +0200

    removed var keyword

--- test/utils.test.ts
+++ test/utils.test.ts
@@ -1,12 +1,12 @@
-const BNasn1 = require('asn1.js').bignum;
+const bignum = require('asn1.js').bignum;
 import { bnToBase64Url } from '../src/utils';
 import BN from "bn.js";
 
 // copied from https://github.com/Brightspace/node-jwk-to-pem/blob/master/src/b64-to-bn.js
 // to compare against this implementation
 function base64ToBigNum(val: string) {
-    var buf = Buffer.from(val, 'base64');
-    var bn = val = new BNasn1(buf, 10, 'be').iabs();
+    const buf = Buffer.from(val, 'base64');
+    const bn = val = new bignum(buf, 10, 'be').iabs();
     return bn;
 };
 
