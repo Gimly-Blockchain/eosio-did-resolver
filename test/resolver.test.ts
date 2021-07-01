129d9c6d4b85cbe09c6d442f2423f027d9e7a81d
jay <julius.rahaus@qciety.org>
2021-07-01 20:00:51 +0200

    fix then syntax to async await

--- test/resolver.test.ts
+++ test/resolver.test.ts
@@ -14,42 +14,42 @@ describe('resolver tests', () => {
   }));
   describe('Dids resolve to DIDResolutionResult', () => {
     responses.forEach(({ did, asyncResponse }) => {
-      it(`did: ${did}`, () =>
-        asyncResponse.then(response => {
-          expect(response).toHaveProperty('didDocument');
-          expect(response).toHaveProperty('didDocumentMetadata');
-          expect(response).toHaveProperty('didResolutionMetadata');
-        }));
+      it(`did: ${did}`, async () => {
+        const response = await asyncResponse;
+        expect(response).toHaveProperty('didDocument');
+        expect(response).toHaveProperty('didDocumentMetadata');
+        expect(response).toHaveProperty('didResolutionMetadata');
+      });
     });
   });
   describe('didResolutionMetadata matches contentType if didDocument is present', () => {
     responses.forEach(({ did, asyncResponse }) => {
-      it(`did: ${did}`, () =>
-        asyncResponse.then(response => {
-          if (response.didDocument)
-            expect(response.didResolutionMetadata).toEqual({
-              contentType: 'application/did+ld+json',
-            });
-        }));
+      it(`did: ${did}`, async () => {
+        const response = await asyncResponse;
+        if (response.didDocument)
+          expect(response.didResolutionMetadata).toEqual({
+            contentType: 'application/did+ld+json',
+          });
+      });
     });
   });
   describe('didResolutionMetadata has error if and only if didDocument is null', () => {
     responses.forEach(({ did, asyncResponse, expectedResult }) => {
-      it(`did: ${did}`, () =>
-        asyncResponse.then(response => {
-          if (response.didDocument === null)
-            expect(response.didResolutionMetadata).toHaveProperty('error');
-          else if ('error' in response.didResolutionMetadata)
-            expect(response.didDocument).toEqual(null);
-        }));
+      it(`did: ${did}`, async () => {
+        const response = await asyncResponse;
+        if (response.didDocument === null)
+          expect(response.didResolutionMetadata).toHaveProperty('error');
+        else if ('error' in response.didResolutionMetadata)
+          expect(response.didDocument).toEqual(null);
+      });
     });
   });
   describe('response matches expected results exactly', () => {
     responses.forEach(({ did, asyncResponse, expectedResult }) => {
-      it(`did: ${did}`, () =>
-        asyncResponse.then(response => {
-          expect(response).toEqual(expectedResult);
-        }));
+      it(`did: ${did}`, async () => {
+        const response = await asyncResponse;
+        expect(response).toEqual(expectedResult);
+      });
     });
   });
 });
